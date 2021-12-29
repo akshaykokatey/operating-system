8a398cf237e8e20db26433d05d52bc46e4d01bba
Akshay kokatey <96278403+akshaykokatey@users.noreply.github.com>
2021-12-29 22:39:47 +0530

    Create Fcfs.c

---
+++ Fcfs.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+ #include<stdlib.h> 
+
+int main()
+
+{
+
+ printf("Program is created by Akshay kokatey\n");
+
+int RQ[100],i,n,TotalHeadMoment=0,initial;
+
+ printf("Enter the number of Requests\n");
+
+ scanf("%d",&n);
+
+ printf("Enter the Requests sequence\n");
+
+ for(i=0;i<n;i++)scanf("%d",&RQ[i]);
+
+ printf("Enter initial head position\n");
+
+ scanf("%d",&initial);
+
+ // logic for FCFS disk schedulingfor
+
+ for(i=0;i<n;i++)
+
+{
+
+TotalHeadMoment=TotalHeadMoment+abs(RQ[i]-initial);
+
+initial=RQ[i];
+
+}
+
+ printf("Total head moment is %d",TotalHeadMoment);
+
+ return 0;
+
+ }
